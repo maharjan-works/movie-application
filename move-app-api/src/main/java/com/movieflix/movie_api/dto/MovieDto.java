52d70f6e45c99f1d2c37e76bf1ae47bb44e87b3f
maharjan.works <maharjan.works@gmail.com>
2025-01-24 10:32:44 -0500

    file-handling-service - upload and serve methods added

--- move-app-api/src/main/java/com/movieflix/movie_api/dto/MovieDto.java
+++ move-app-api/src/main/java/com/movieflix/movie_api/dto/MovieDto.java
@@ -1,41 +1,39 @@
-package com.movieflix.movie_api.model;
+package com.movieflix.movie_api.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Set;
 
-@Entity
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-public class Movie {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class MovieDto {
+
+
     private Integer id;
-    @Column(nullable = false) // database level validation
+
     @NotBlank(message="movie title is missing") // application level validation
     private String title;
-    @Column(nullable = false)
+
     @NotBlank(message="director name is missing")
     private String director;
-    @Column(nullable = false)
+
     @NotBlank(message="studio name is missing")
     private String studio;
 
-    @ElementCollection
-    @CollectionTable(name = "movie_cast")
+    @NotBlank(message = "movie cast are missing")
     private Set<String> movieCast;
 
-    @Column(nullable=false)
     private Integer releaseYear;
 
-    @Column(nullable=false)
     @NotBlank(message="poster is messing")
     private String poster;
+
+    @NotBlank(message = "poster url is missing")
+    private String posterUrl;
+
 }
