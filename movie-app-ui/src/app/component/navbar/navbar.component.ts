4685e0a34b1f2caf9af4d3337f34383af55b888d
maharjan.works <maharjan.works@gmail.com>
2025-02-19 19:46:26 -0500

    ui - navbar updated with authenticated user

--- movie-app-ui/src/app/component/navbar/navbar.component.ts
+++ movie-app-ui/src/app/component/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +11,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  name = sessionStorage.getItem('name');
+  isLoggedIn = signal<boolean>(false);
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  ngOnInit(): void{
+    this.isLoggedIn = this.authService.getLoggedIn();
+  }
+
+
+  logout(){
+    this.authService.logout();
+    this.authService.setLoggedIn(false);
+    this.router.navigate(['login']);
+  }
 
 }
--- movie-app-ui/src/app/service/auth.service.ts
+++ movie-app-ui/src/app/service/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +8,50 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private BASE_URL = 'http://localhost:8080/api/v1/auth';
-  constructor(private http: HttpClient) { }
+  private loggedIn = signal<boolean>(this.isAuthenticated());
 
+  constructor(private http: HttpClient) { }
 
   register(registerRequest: RegisterRequest): Observable<AuthResponse>{
     return this.http.post<AuthResponse>(`${this.BASE_URL}/register`, registerRequest);
   }
 
   login(loginRequest: LoginRequest): Observable<AuthResponse>{
-    return this.http.post<AuthResponse>(`${this.BASE_URL}/login`, loginRequest);
+    return this.http.post<AuthResponse>(`${this.BASE_URL}/login`, loginRequest).pipe(
+      tap(response => {
+        if (response && response.accessToken){
+          sessionStorage.setItem('accessToken', response.accessToken);
+          sessionStorage.setItem('refreshToken', response.refreshToken);
+          sessionStorage.setItem('name', response.firstName + ' '+ response.lastName);
+          sessionStorage.setItem('email', response.email);
+          sessionStorage.setItem('username', response.username);
+          this.setLoggedIn(true);
+        }
+      })
+    );
+  } //end of login()
+
+
+  logout(): void{
+    sessionStorage.removeItem('accessToken');
+    sessionStorage.removeItem('refreshToken');
+    sessionStorage.removeItem('name');
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('username');
   }
+
+  isAuthenticated():boolean{
+    return sessionStorage.getItem('accessToken') != null;
+  }
+
+  setLoggedIn(value: boolean){
+    this.loggedIn.set(value);
+  }
+
+  getLoggedIn(): WritableSignal<boolean>{
+    return this.loggedIn;
+  }
+
 
 }
 
